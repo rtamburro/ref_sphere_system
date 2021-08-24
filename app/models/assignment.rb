c3885a982f5605c9871ee9152dd7a0685ad41635
Ronnie Tamburro <ronnie.tamburro@protonmail.com>
2021-08-24 19:30:47 -0400

    associations added to models

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,7 +1,7 @@
 class Assignment < ApplicationRecord
   belongs_to :game
-  belongs_to :center_referee
-  belongs_to :assistant_referee_1
-  belongs_to :assistant_referee_2
-  belongs_to :fourth_official
+  belongs_to :center_referee, class_name: "User"
+  belongs_to :assistant_referee_1, class_name: "User"
+  belongs_to :assistant_referee_2, class_name: "User"
+  belongs_to :fourth_official, class_name: "User"
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+    has_one :assignment
+    has_many :users, through: :assignment
 end
